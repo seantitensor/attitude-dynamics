f9b90826b3e7c1fc2d3988b8ed5534216b9fc6b7
rhyuj <rhyuj@DESKTOP-D3253F1>
2026-04-16 21:08:43 -0600

    basic engine built

--- code-rhyu/FinalProjectp330.m
+++ code-rhyu/FinalProjectp330.m
@@ -22,3 +22,10 @@ tspan = [0 100];
 % engine
 options = odeset('RelTol', 1e-8, 'AbsTol', 1e-10);
 [t, X] = ode45(@(t, X) spacecraft_dynamics(t, X, I, rho, v_orb, Cd, A, r_cp), tspan, X0, options);
+
+% normalize state
+q_out = X(:, 1:4);
+q_squared = q_out.^2;
+sum_sq = sum(q_squared, 2);
+mag = sqrt(sum_sq);
+q_out = q_out ./ mag;
--- code-rhyu/spacecraft_dynamics.m
+++ code-rhyu/spacecraft_dynamics.m
@@ -1,5 +1,22 @@
 function dX = spacecraft_dynamics(t, X, I, rho, v_orb, Cd, A, r_cp)
     % state variables
     q = X(1:4); 
-    w = X(5:7); 
+    w = X(5:7);
+
+    % rotation 
+    Omega = [0, -w(1), -w(2), -w(3);
+             w(1),  0,   w(3), -w(2);
+             w(2), -w(3), 0,    w(1);
+             w(3),  w(2), -w(1), 0];
+    dq = 0.5 * Omega * q;
+
+    % drag force
+    F_drag_mag = 0.5 * rho * v_orb^2 * Cd * A;
+    F_drag_vec = [0; 0; -F_drag_mag];
+
+    % torque and dynamics
+    tau_aero = cross(r_cp, F_drag_vec);
+    dw = I \ (tau_aero - cross(w, I * w));
+    
+    dX = [dq; dw];
 end
