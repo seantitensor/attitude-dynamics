b7e0ac11070afc7d58ec3df48ec748d11e70112a
rhyuj <rhyuj@DESKTOP-D3253F1>
2026-04-16 21:01:00 -0600

    params added

--- code-rhyu/FinalProjectp330.m
+++ code-rhyu/FinalProjectp330.m
@@ -1,1 +1,24 @@
-% main script
+clc; clear; close all;
+
+% parameters 
+% inertia tensor
+I = diag([10, 15, 20]); 
+
+% environ constant
+rho = 1.225e-12;
+v_orb = 7800;
+Cd = 2.2;
+A = 2.0;
+r_cp = [0.2; 0.05; 0];
+
+% state variables
+q0 = [1; 0; 0; 0]; 
+w0 = [0.1; -0.05; 0.02]; 
+X0 = [q0; w0];
+
+% time
+tspan = [0 100];
+
+% engine
+options = odeset('RelTol', 1e-8, 'AbsTol', 1e-10);
+[t, X] = ode45(@(t, X) spacecraft_dynamics(t, X, I, rho, v_orb, Cd, A, r_cp), tspan, X0, options);
--- code-rhyu/spacecraft_dynamics.m
+++ code-rhyu/spacecraft_dynamics.m
@@ -1,1 +1,5 @@
-% rhs todo
+function dX = spacecraft_dynamics(t, X, I, rho, v_orb, Cd, A, r_cp)
+    % state variables
+    q = X(1:4); 
+    w = X(5:7); 
+end
