482d8e4d4178676b046fea4163eb7e5cb7ec0f77
rhyuj <rhyuj@DESKTOP-D3253F1>
2026-04-17 11:15:41 -0600

    testing cases and debugging

--- code-rhyu/FinalProjectp330.m
+++ code-rhyu/FinalProjectp330.m
@@ -1,32 +1,34 @@
-% clc; clear; close all;
+clc; clear; close all;
 
 % parameters 
 % environ constant
-rho = 1.225;
+rho = 1.225e-3;
 v_orb = 7800;
 Cd = 2.2;
 
 % state variables
-M = 50;
-L = 100;
+M = 10;
 q0 = [1; 0; 0; 0]; 
-w0 = [0.5; 0; 0]; 
-A = L^2;
-h = L/2;
-geom = [1, 0, 0,  A,  h-20, 0, 0;  % +X Face
-        -1, 0, 0,  A, -h-20, 0, 0;  % -X Face
-        0, 1, 0,  A,  0-20, h, 0;  % +Y Face
-        0,-1, 0,  A,  0-20,-h, 0;  % -Y Face
-        0, 0, 1,  A,  0, 0, h;  % +Z Face
-        0, 0,-1,  A,  0, 0,-h]; % -Z Face
+w0 = [0; 0; 0];
 X0 = [q0; w0];
+a = 6; b = 12; c = 4;
+off = 2;
+geom = [ 1, 0, 0,  b*c,  a/2 + off, 0, 0;   % +X
+        -1, 0, 0,  b*c, -a/2 + off, 0, 0;   % -X
+         0, 1, 0,  a*c,  off, b/2, 0;       % +Y
+         0,-1, 0,  a*c,  off, -b/2, 0;      % -Y
+         0, 0, 1,  a*b,  off, 0, c/2;       % +Z
+         0, 0,-1,  a*b,  off, 0, -c/2];     % -Z
+
+% Inertia
+I = diag([ (1/12)* M *(b^2+c^2), (1/12)* M *(a^2+c^2), (1/12)* M *(a^2+b^2) ]);
 
 % inertia tensor
-I_val = (1/6) * M * L^2;
-I = diag([I_val, I_val, I_val]); 
+%I_val = (1/6) * M * L^2;
+%I = diag([I_val, I_val, I_val]); 
 
 % time
-tspan = [0 500];
+tspan = [0 15000];
 
 % engine
 options = odeset('RelTol', 1e-8, 'AbsTol', 1e-10);
